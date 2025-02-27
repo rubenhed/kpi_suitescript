26e3ab5b385ca4eec42f2b71a6a79ea6e527ed56
Ruben Hedstrom/ルーベン・ヘドストロム <ruben.hedstrom@gadelius.com>
2025-02-27 15:16:08 +0900

    .

--- kpi_calc.js
+++ kpi_calc.js
@@ -3,131 +3,105 @@
  * @NScriptType Portlet
  */
 
-MONTHS_TO_DISPLAY = 6;
+MONTHS_TO_DISPLAY = 7;
 
-define(['N/ui/serverWidget', 'N/search'], (serverWidget, search) => {
+//'N/ui/serverWidget',  serverWidget, 
+define(['N/search'], (search) => {
   let html = "";
-  
-  const getCapital = (savedSearchId) => { //正味運転資本額
+  const finalResults = {};
+  const orderedDates = [];
+
+  const getSearchRange = (savedSearchId) => { //gets 1000(max) latest results.
     const mySearch = search.load({ id: savedSearchId });
     const resultSet = mySearch.run();
-    const range = resultSet.getRange({ start: 0, end: 200 }); //65 items as of 20250220
+    const range = resultSet.getRange({ start: 0, end: 1000 });
+    return range;
+  }
 
-    const finalResults = {};
-    const orderedDates = [];
+  const getCapital = (savedSearchId) => { //正味運転資本額
+    const range = getSearchRange(savedSearchId); //65 items as of 20250220
     let capitalTotal = 0;
 
     range.reverse().forEach((result) => {
       const tranDate = result.getValue({ name: 'trandate', summary: search.Summary.GROUP }) || 'No Date found';
-
       const formulaCurrencyMonth = result.getValue({ name: 'formulacurrency', summary: search.Summary.SUM }) || 0;
-
       capitalTotal += parseFloat(formulaCurrencyMonth) || 0;
       
       finalResults[tranDate] = { capitalTotal };
       orderedDates.push(tranDate);
     });
-    
-    return [orderedDates.reverse(), finalResults]; //to here
+
+    orderedDates.reverse();
   };
 
-  const getAverage = (orderedDates, finalResults) => {
-    //const capitalCurrency = capitalResults.map(capitalResults => capitalResults.formulaCurrencyTotal);
+  const getCapitalAverage = () => { //正味運転資本額 (R12)
     const AVERAGE_PERIOD = 2;
-    //const averageResults = {};
+
     for (let i = 0; i < MONTHS_TO_DISPLAY; i++) {
       const keys = orderedDates.slice(i, i + AVERAGE_PERIOD);
       const values = keys.map(key => finalResults[key]?.capitalTotal || 0);
       const sum = values.reduce((acc, value) => acc + value, 0);
       const capitalAverage = sum / AVERAGE_PERIOD; 
-      //const average = capitalCurrency.slice(i, i + AVERAGE_PERIOD).reduce((acc, value) => acc + value, 0) / AVERAGE_PERIOD;
+
       finalResults[orderedDates[i]].capitalAverage = capitalAverage;
     }
-
-    return finalResults;
   };
 
-  const getInventory = (savedSearchId, finalResults) => { //棚卸資産
-    const mySearch = search.load({ id: savedSearchId });
-    const resultSet = mySearch.run();
-    const range = resultSet.getRange({ start: 0, end: 200 }); //7 items as of 20250220
-
-    const results = [];
+  const getInventory = (savedSearchId) => { //棚卸資産
+    const range = getSearchRange(savedSearchId); //7 items as of 20250220
     let inventoryCurrencyTotal = 0;
 
     range.reverse().forEach((result) => {
       const tranDate = result.getValue({ name: 'trandate', summary: search.Summary.GROUP }) || 'No Date found';
-
       const formulaCurrencyMonth = result.getValue({ name: 'formulacurrency', summary: search.Summary.SUM }) || 0;
-
       inventoryCurrencyTotal += parseFloat(formulaCurrencyMonth) || 0;
+
       if (finalResults[tranDate]){
         finalResults[tranDate].inventory = inventoryCurrencyTotal;
       }
-      
-
-      //results.push({ tranDate, formulaCurrencyTotal });
     });
-    
-    return finalResults;
   };
 
-  const getDepreciation = (savedSearchId, finalResults) => { //減価償却費
-    const mySearch = search.load({ id: savedSearchId });
-    const resultSet = mySearch.run();
-    const range = resultSet.getRange({ start: 0, end: 200 }); //7 items as of 20250220
+  const getDepreciation = (savedSearchId) => { //減価償却費
+    const range = getSearchRange(savedSearchId); //7 items as of 20250220
 
     range.forEach((result) => {
       const tranDate = result.getValue({ name: 'trandate', summary: search.Summary.GROUP }) || 'No Date found';
-
       const depreciationCurrencyMonth = result.getValue({ name: 'formulacurrency', summary: search.Summary.SUM }) || 0;
 
-      //results.push({ tranDate, depreciationCurrencyMonth : parseFloat(depreciationCurrencyMonth) });
       if (finalResults[tranDate]){
         finalResults[tranDate].depreciation = parseFloat(depreciationCurrencyMonth);
       }
     });
-
-    return finalResults;
   };
 
-  const getCostOfGoods = (savedSearchId, finalResults) => { //売上原価and営業利益 id is 10 and 26
-    const mySearch = search.load({ id: savedSearchId });
-    const resultSet = mySearch.run();
-    const range = resultSet.getRange({ start: 0, end: 200 }); //15 items as of 20250220
+  const getCostOfGoods = (savedSearchId) => { //売上原価 id is 10
+    const range = getSearchRange(savedSearchId); //15 items as of 20250220
 
     range.forEach((result) => {
       const tranDate = result.getValue({ name: 'formulatext' }) || 'No Date found';
-
       const currentMonth = result.getValue({ name: 'custrecord_formatc_current_month_actual' }) || 0;
-      
 
-      //results.push(tranDate);
       if (finalResults[tranDate]){
-        finalResults[tranDate].cog = currentMonth;
+        finalResults[tranDate].cog = parseInt(currentMonth) * -1; //all costs are negative, display as positive number
       }
     });
   }
 
-  const getOperatingIncome = (savedSearchId, finalResults) => { //売上原価and営業利益 id is 10 and 26
-    const mySearch = search.load({ id: savedSearchId });
-    const resultSet = mySearch.run();
-    const range = resultSet.getRange({ start: 0, end: 200 }); //15 items as of 20250220
+  const getOperatingIncome = (savedSearchId) => { //営業利益 id is 26
+    const range = getSearchRange(savedSearchId); //15 items as of 20250220
 
     range.forEach((result) => {
       const tranDate = result.getValue({ name: 'formulatext' }) || 'No Date found';
-
       const currentMonth = result.getValue({ name: 'custrecord_formatc_current_month_actual' }) || 0;
-      
 
-      //results.push(tranDate);
       if (finalResults[tranDate]){
         finalResults[tranDate].oi = currentMonth;
       }
     });
   }
 
-  const getCashflow = (orderedDates, finalResults) => { //capitalTotal + depreciation + oi
+  const getCashflow = () => { //capitalTotal + depreciation + oi 営業キャッシュフロー
     for (let i = 0; i < MONTHS_TO_DISPLAY; i++) {
       const date = orderedDates[i];
       const result = finalResults[date];
@@ -141,88 +115,114 @@ define(['N/ui/serverWidget', 'N/search'], (serverWidget, search) => {
     }
   }
 
+  const getInvTurnover = () => { //棚卸資産回転率(原価)
+    const AVERAGE_PERIOD = 12;
+    
+    for (let i = 0; i < MONTHS_TO_DISPLAY; i++) {
+      let costOfGood = 0;
+      let totalInventory = 0;
+      const finalResult = finalResults[orderedDates[i]];
+
+      for (let j = i; j < AVERAGE_PERIOD + i; j++) {
+        const result = finalResults[orderedDates[j]];
+        costOfGood += parseInt(result.cog) || 0;
+        totalInventory += parseInt(result.inventory) || 0;
+      }
+
+      const averageInventory = totalInventory / AVERAGE_PERIOD;
+      finalResult.inventoryTurnoverAverage = costOfGood / averageInventory;
+
+      finalResult.inventoryTurnoverThisMonth = costOfGood / parseInt(finalResult.inventory);
+    }
+  }
+
+  const addStyling = () => {
+    html += `
+      <style>
+        #kpi-table {
+          width: 100%;
+          border-collapse: collapse;
+          text-align: left;
+        }
+        #kpi-table th, #kpi-table td {
+          border: 1px solid black;
+          padding: 8px;
+        }
+        #kpi-table th {
+          background-color:rgb(242, 242, 242);
+        }
+      </style>`;
+  }
+
   const render = (params) => {
     const portlet = params.portlet;
 
     // https://6317455.app.netsuite.com/app/common/search/searchresults.nl?searchid=8705
     const capitalSavedSearchId = '8705';
-    const [orderedDates, finalResults] = getCapital(capitalSavedSearchId);
-
-    getAverage(orderedDates, finalResults);
+    getCapital(capitalSavedSearchId);
+    getCapitalAverage();
 
     // https://6317455.app.netsuite.com/app/common/search/searchresults.nl?searchid=8707
     const inventorySavedSearchId = '8707';
-    getInventory(inventorySavedSearchId, finalResults);
+    getInventory(inventorySavedSearchId);
 
     // https://6317455.app.netsuite.com/app/common/search/searchresults.nl?searchid=8708
     const depreciationSavedSearchId = '8708';
-    getDepreciation(depreciationSavedSearchId, finalResults);
-
-    // dates = capitalResults.map(capitalResults => capitalResults.tranDate);
+    getDepreciation(depreciationSavedSearchId);
 
     // https://6317455.app.netsuite.com/app/common/search/searchresults.nl?searchid=8718
     const goodsCostSavedSearchId = '8718';
-    getCostOfGoods(goodsCostSavedSearchId, finalResults);
-    //portlet.html = test.join(', ');
-    //return;
+    getCostOfGoods(goodsCostSavedSearchId);
 
     // https://6317455.app.netsuite.com/app/common/search/searchresults.nl?searchid=8719
     const operatingIncomeSavedSearchId = '8719';
-    getOperatingIncome(operatingIncomeSavedSearchId, finalResults);
+    getOperatingIncome(operatingIncomeSavedSearchId);
 
-    getCashflow(orderedDates, finalResults);
+    getCashflow();
 
+    getInvTurnover();
 
-    // Create the HTML content with styling
+    addStyling();
+    html += `<table id="kpi-table">`;
     html += `
-      <style>
-        table {
-          width: 100%;
-          border-collapse: collapse;
-        }
-        th, td {
-          border: 1px solid black;
-          padding: 8px;
-          text-align: left;
-        }
-        th {
-          background-color: #f2f2f2;
-        }
-      </style>`;
-
-    html += `<table>`;
-    html += `<tr>
-      <th>日付</th>
-      <th><a href="https://6317455.app.netsuite.com/app/common/search/searchresults.nl?searchid=8705" target="_blank">正味運転資本額</a></th>
-      <th>正味運転資本額 (R12) (2 months)</th>
-      <th><a href="https://6317455.app.netsuite.com/app/common/search/searchresults.nl?searchid=8707" target="_blank">棚卸資産</a></th>
-      <th><a href="https://6317455.app.netsuite.com/app/common/search/searchresults.nl?searchid=8708" target="_blank">減価償却費</a></th>
-      <th><a href="https://6317455.app.netsuite.com/app/common/search/searchresults.nl?searchid=8718" target="_blank">売上原価</a></th>
-      <th><a href="https://6317455.app.netsuite.com/app/common/search/searchresults.nl?searchid=8719" target="_blank">営業利益</a></th>
-      <th>営業キャッシュフロー</th>
-    </tr>`;
+      <tr>
+        <th>日付</th>
+        <th><a href="https://6317455.app.netsuite.com/app/common/search/searchresults.nl?searchid=8705" target="_blank">正味運転資本額</a></th>
+        <th>正味運転資本額 (R12) (2 months)</th>
+        <th><a href="https://6317455.app.netsuite.com/app/common/search/searchresults.nl?searchid=8707" target="_blank">棚卸資産</a></th>
+        <th><a href="https://6317455.app.netsuite.com/app/common/search/searchresults.nl?searchid=8708" target="_blank">減価償却費</a></th>
+        <th><a href="https://6317455.app.netsuite.com/app/common/search/searchresults.nl?searchid=8718" target="_blank">売上原価</a></th>
+        <th><a href="https://6317455.app.netsuite.com/app/common/search/searchresults.nl?searchid=8719" target="_blank">営業利益</a></th>
+        <th>営業キャッシュフロー</th>
+        <th>売上原価(過去12ヵ月合計) / 棚卸資産(12ヵ月平均)</th>
+        <th>売上原価(過去12ヵ月合計) / 棚卸資産(当月残)</th>
+      </tr>`;
 
     for (let i = 0; i < MONTHS_TO_DISPLAY; i++) {
       const date = orderedDates[i];
       const result = finalResults[date];
 
       const capital = result.capitalTotal;
-      const average = result.capitalAverage;
+      const capitalAverage = result.capitalAverage;
       const inventory = result.inventory;
       const depreciation = result.depreciation;
       const cog = result.cog;
       const oi = result.oi;
       const cashFlow = result.cashFlow;
+      const inventoryTurnoverAverage = result.inventoryTurnoverAverage;
+      const inventoryTurnoverThisMonth = result.inventoryTurnoverThisMonth;
 
       html += `<tr>
         <td>${date}</td>
         <td>${capital?.toLocaleString()}¥</td>
-        <td>${parseInt(average)?.toLocaleString()}¥</td>
+        <td>${parseInt(capitalAverage)?.toLocaleString()}¥</td>
         <td>${inventory?.toLocaleString()}¥</td>
         <td>${depreciation?.toLocaleString()}¥</td>
         <td>${parseInt(cog)?.toLocaleString()}¥</td>
         <td>${parseInt(oi)?.toLocaleString()}¥</td>
         <td>${cashFlow?.toLocaleString()}¥</td>
+        <td>${inventoryTurnoverAverage?.toLocaleString()}</td>
+        <td>${inventoryTurnoverThisMonth?.toLocaleString()}</td>
       </tr>`;
     }
 
